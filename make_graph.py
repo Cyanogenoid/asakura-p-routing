5b86252e5ad9f0656359f7d99492210748eeb2c4
Yan Zhang <cyanogenoid@cyanogenoid.com>
2017-04-04 18:48:12 +0200

    Implement drawing the floor graph

--- make_graph.py
+++ make_graph.py
@@ -1,5 +1,30 @@
+import pydot_ng as pd
+
 from load import load_movedata
 
 
-doors = load_movedata('MoveData.dat')
+def create_graph(doors):
+    graph = pd.Dot(graph_type='graph')
 
+    node_format = '{}F'.format
+    nodes = [pd.Node(node_format(i)) for i in doors.keys()]
+    for node in nodes:
+        graph.add_node(node)
+
+    for floor, ds in doors.items():
+        floor_node = nodes[floor - 1]
+        for target_floor, _ in ds:
+            if target_floor == 101:
+                d_node = pd.Node('Credits')
+                graph.add_node(d_node)
+            else:
+                d_node = nodes[target_floor - 1]
+            edge = pd.Edge(floor_node, d_node)
+            graph.add_edge(edge)
+
+    return graph
+
+
+doors = load_movedata('MoveData.dat')
+graph = create_graph(doors)
+graph.write_png('test.png')
