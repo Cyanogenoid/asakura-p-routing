479ed8dda675112438e688d612fc9eba3f21bf9b
Yan Zhang <cyanogenoid@cyanogenoid.com>
2017-04-04 22:33:43 +0200

    Adjust node styling, add edge styling

--- make_graph.py
+++ make_graph.py
@@ -1,3 +1,5 @@
+from itertools import tee
+
 import pydot_ng as pd
 
 from load import load_all
@@ -9,10 +11,29 @@ def apply_style(floor, map):
          6: {'color': 'orange'},
         29: {'fillcolor': 'yellow'},
     }.get(map['chest'][-1], {})
-    shop_style = {'fillcolor': 'orange'} if floor in [8, 43, 63, 97] else {}
+    shop_style = {'fillcolor': 'orange'} if floor in {8, 43, 63, 97} else {}
+    boss_style = {'fillcolor': 'gray'} if floor in {10, 25, 40, 55, 70, 85, 100} else {}
 
     style.update(chest_style)
     style.update(shop_style)
+    style.update(boss_style)
+    return style
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+any_route = set(pairwise([1,2,3,4,5,6,8,9,10,11,12,18,13,18,12,14,15,19,20,21,24,25,26,27,28,31,32,33,34,35,39,40,41,45,46,50,52,51,46,48,49,41,50,52,55,56,58,60,62,60,58,61,65,67,70,71,73,75,76,81,82,84,82,81,76,75,73,85,98,99,86,87,88,98,99,100,101]))
+def apply_edge_style(a, b):
+    style = {}
+    if (a, b) in any_route or (b, a) in any_route:
+        any_route.discard((a, b))
+        any_route.discard((b, a))
+        style['color'] = 'red'
     return style
 
 
@@ -38,8 +59,10 @@ def create_graph(maps):
         for target_floor, _ in map['targets']:
             if target_floor < floor:
                 continue
+            style = {}
+            style.update(apply_edge_style(floor, target_floor))
             target_node = nodes[target_floor]
-            edge = pd.Edge(floor_node, target_node)
+            edge = pd.Edge(floor_node, target_node, **style)
             graph.add_edge(edge)
 
     return graph
