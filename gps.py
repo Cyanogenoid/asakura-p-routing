2e0cc4e92cf988299f05d8ac1bd9fab61d7724e0
Yan Zhang <cyanogenoid@cyanogenoid.com>
2020-06-28 15:08:55 +0200

    Finish GPS

--- gps.py
+++ gps.py
@@ -1,9 +1,11 @@
+import argparse
 import time
-import inject
 
 import psutil
 import colorama
 from colorama import Fore, Back, Style
+
+import inject
 
 colorama.init()
 
@@ -12,6 +14,8 @@ class Game(object):
     def __init__(self, process):
         self.process = process
         self.item_address = 0x5DA6AC
+        self.boss_floors = [10, 25, 40, 55, 70, 85, 100]
+        self.boss_address = 0x5DABDC
 
     def floor(self):
         return self._read(0x5D9AFC) + 1
@@ -46,8 +50,20 @@ class Game(object):
         offset = 4 * ((floor - 1) * 3 + 2)
         return self._read(self.item_address + offset) == 1
     
-    def boss_explosions_playing(self):
-        return self._read(0x5DAC30) > 0
+    def boss_beaten(self, floor):
+        if floor not in self.boss_floors:
+            return False
+        offset = self.boss_address + 4 * self.boss_floors.index(floor)
+        return self._read(offset) == 1
+    
+    def menu_open(self):
+        return self._read(0x5DAC48) == 1
+
+    def menu_selection(self):
+        return self._read(0x5B4150) + 1
+
+    def shop_open(self):
+        return self._read(0x5DAC48) == 1
 
     def _read(self, offset):
         return self.process.read_int32(offset)
@@ -60,6 +76,10 @@ class Entry(object):
         self.actions = actions
         self.comment = comment
 
+        self.action_index = 0
+        self.menu_success = False
+        self.shop_success = False
+
 
 class GPS(object):
     COLOURS = {
@@ -71,23 +91,25 @@ class GPS(object):
         'w': Fore.MAGENTA,
     }
 
-    def __init__(self, game, route):
+    def __init__(self, game, route, start_from_line=1, context_future=10, context_past=5):
+        self.start_from_line = start_from_line
+        self.context_past = context_past
+        self.context_future = context_future
+
         self.game = game
         self.route = self._parse(route)
-        self.current_line = 0
-        self.current_floor = 0
 
     def _parse(self, route):
         floors = []
         section = ''
-        for line in route:
+        for i, line in enumerate(route):
             if not line:
                 continue
             if line.startswith('# '):
                 section = line
             elif line.startswith('status'):
                 pass
-            else:
+            elif i + 1 >= self.start_from_line:
                 parts = line.split('#')
                 if len(parts) >= 2:
                     comment = ' '.join(x.strip() for x in parts[1:])
@@ -104,55 +126,113 @@ class GPS(object):
                     
     def run(self):
         done = False
-        prev_items = (None, None, None)
-        for entry in self.route:
-            # TODO support not advancing when going into the wrong door
-            action_index = 0
+        last_output = None
+        past_current_floors = []
+        print(f'Waiting for {self.route[0].floor}F')
+        for entry_index, entry in enumerate(self.route):
+            while entry.floor != self.game.floor():
+                time.sleep(0.1) 
             while entry.floor == self.game.floor():
-                b = self.game.has_bubble_key(entry.floor) or (self.game.boss_explosions_playing() and entry.floor in {10, 25, 40, 55, 70, 85, 100})
-                k = self.game.has_key(entry.floor)
-                c = self.game.has_chest(entry.floor)
-                r = self.game.is_dead()
-                
-                floor_complete = action_index >= len(entry.actions)
-                if not floor_complete:
-                    next_action = entry.actions[action_index]
-                    conditions = [
-                        next_action == 'b' and b,
-                        next_action == 'c' and c,
-                        next_action == 'k' and k,
-                        next_action == 'r' and r,
-                    ]
-                    if any(conditions):
-                        action_index += 1
-                        floor_complete = action_index >= len(entry.actions)
-                
-                floor_output = f'{Fore.GREEN if floor_complete else Fore.RED}{entry.floor}{Fore.WHITE}F'
-                action_output = self.format_actions(entry.actions, action_index)
-                output = f'{Style.BRIGHT}{floor_output} {action_output}{Style.RESET_ALL}'
-                print(output)
+                output_lines = []
+
+                output_lines.append(f'\033[2J{entry.section}')
+                output_lines.append('')
+
+                entries_before = self.route[max(entry_index - self.context_past, 0):entry_index]
+                output_lines += (self.context_past - len(entries_before)) * ['']
+                for e in entries_before:
+                    output_lines.append(self.format_entry(e))
+
+                next_entry = self.route[entry_index + 1] if entry_index + 1 < len(self.route) else ''
+                output_lines.append('')
+                output_lines.append(self.format_entry(entry, next_entry=next_entry, style=Style.BRIGHT))
+                output_lines.append('')
+
+                current_section = entry.section
+                entries_after = self.route[entry_index + 1:entry_index + 1 + self.context_future]
+                new_lines = []
+                for e in entries_after:
+                    if e.section != current_section:
+                        current_section = e.section
+                        new_lines.append(e.section)
+                    new_lines.append(self.format_entry(e, coloured_floor=False))
+                output_lines += new_lines[:self.context_future]
+
+                output = '\n'.join(output_lines)
+                if output != last_output:
+                    last_output = output
+                    print(output)
 
                 time.sleep(0.1)
+
+    def format_entry(self, entry, next_entry=None, style=Style.NORMAL, coloured_floor=True):
+        b = self.game.has_bubble_key(entry.floor) or self.game.boss_beaten(entry.floor)
+        k = self.game.has_key(entry.floor)
+        c = self.game.has_chest(entry.floor)
+        r = self.game.is_dead()
+        entry.menu_success = entry.menu_success or (self.game.menu_open() and next_entry and self.game.menu_selection() == next_entry.floor)
+        entry.shop_success = entry.shop_success or self.game.shop_open()
         
-    def format_actions(self, actions, num_complete):
+        # any actions left?
+        if entry.action_index < len(entry.actions):
+            next_action = entry.actions[entry.action_index]
+            condition = {
+                'b': b,
+                'c': c,
+                'k': k,
+                'r': r,
+                't': entry.menu_success,
+                'w': entry.menu_success,
+                's': entry.shop_success,
+                '?': True,
+            }
+            if condition[next_action]:
+                entry.action_index += 1
+        # floor_complete = (not 'b' in entry.actions or b) and (not 'k' in entry.actions or k) and (not 'c' in entry.actions or c)
+        floor_complete = entry.action_index == len(entry.actions)
+
+        floor_colour = Fore.RED if not coloured_floor else Fore.GREEN if floor_complete else Fore.RED
+        floor_output = f'{Style.BRIGHT}{floor_colour}{entry.floor}{Fore.WHITE}{style}F'
+        action_output = self.format_actions(entry.actions, entry.action_index, b=b, k=k, c=c, force_dim=style != Style.BRIGHT)
+        next_floor = f'{Style.BRIGHT} -> {next_entry.floor}F' if next_entry else ''
+        comment = f'  # {entry.comment}' if entry.comment else ''
+        current_floor_string = f'{Style.BRIGHT}{floor_output}{action_output}{Fore.WHITE}{Back.RESET}{next_floor}{Style.RESET_ALL}{comment}'
+        return current_floor_string
+
+    def format_actions(self, actions, num_complete, b, k, c, force_dim=False):
         action_strings = []
+        if not actions:
+            return ''
         for i, action in enumerate(actions):
-            complete = i < num_complete
+            complete = i < num_complete or (action == 'b' and b) or (action == 'k' and k) or (action == 'c' and c)
             if not complete:
-                style = f'{GPS.COLOURS[action]}{Style.BRIGHT}{Back.BLACK}'
+                style = f'{GPS.COLOURS.get(action, Fore.WHITE)}{Style.BRIGHT if not force_dim else Style.DIM}{Back.BLACK}'
             else:
-                style = f'{GPS.COLOURS[action]}{Style.DIM}{Back.GREEN}'
+                style = f'{GPS.COLOURS.get(action, Fore.WHITE)}{Style.DIM}{Back.GREEN}'
             action_strings.append(f'{style}{action}')
-        return ''.join(action_strings)
+        return ' ' + ''.join(action_strings)
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='Path to route')
+parser.add_argument('--process', '-p', default='AssaCrip_en.exe', help='Name of process to read from')
+parser.add_argument('--context-future', '-cf', type=int, default=10, help='Number of upcoming lines to show')
+parser.add_argument('--context-past', '-cp', type=int, default=5, help='Number of past lines to show')
+parser.add_argument('--start-from', '-s', type=int, default=1, help='Start the route <n> lines in (1-indexed)')
+args = parser.parse_args()
 
 
-with open('100.md') as fd:
-    s = fd.read().splitlines()
+with open(args.path) as fd:
+    lines = fd.read().splitlines()
     
 
-with inject.Process(name='AssaCrip_en.exe') as process:
+with inject.Process(name=args.process) as process:
     game = Game(process)
-    gps = GPS(game, route=s)
+    gps = GPS(
+        game,
+        route=lines,
+        start_from_line=args.start_from,
+        context_future=args.context_future,
+        context_past=args.context_past,
+    )
     gps.run()
